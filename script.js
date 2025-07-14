71f2c766b4d42872ef7f411e4f0cd62dc9a4e2b2
Anup Bhattarai <vc81it06@vedascollege.edu.np>
2025-07-14 22:36:17 +0545

    updating header and other key ui improvements

---
+++ script.js
@@ -0,0 +1,7 @@
+        // Mobile menu toggle
+        const menuButton = document.querySelector('.menu-button');
+        const navLinks = document.querySelector('.nav-links');
+
+        menuButton.addEventListener('click', () => {
+            navLinks.classList.toggle('active');
+        });
