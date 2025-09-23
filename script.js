e3344a2572d4ebe9559daa28f61dc23b1dd37ce2
Anup Bhattarai <vc81it06@vedascollege.edu.np>
2025-09-23 10:59:36 +0545

    did some mino ui changes , update links , improvised projectshowcase cards and included another webpage

--- script.js
+++ script.js
@@ -1,26 +1,8 @@
 // Mobile menu toggle
 const menuButton = document.querySelector('.menu-button');
 const navLinks = document.querySelector('.nav-links');
-const toggleBtn = document.getElementById('toggleProjectsBtn');
-const projectCards = document.querySelectorAll('.projects a.card');
 
 menuButton.addEventListener('click', () => {
     navLinks.classList.toggle('active');
-});
-
-toggleBtn.addEventListener('click', () => {
-  if (toggleBtn.textContent === 'Show More') {
-    // Show all projects
-    projectCards.forEach(card => {
-      card.style.display = 'block';
-    });
-    toggleBtn.textContent = 'Show Less';
-  } else {
-    // Show only first 3 projects, hide rest
-    projectCards.forEach((card, index) => {
-      card.style.display = index < 3 ? 'block' : 'none';
-    });
-    toggleBtn.textContent = 'Show More';
-  }
 });
 
